e5d394ce41d551bdde2210b2ddac09da5fb4561c
nur bujang <>
2023-02-06 21:21:28 +0000

    week02 lab 2.2

---
+++ week02/hello.py
@@ -0,0 +1,11 @@
+# hello.py
+# Author: Nur Bujang
+# this just says hello
+
+# print("Hello")
+
+# print("Hello Nur")
+
+print("Hello Nur"
+# SyntaxError: unexpected EOF while parsing
+
---
+++ week02/multiply.py
@@ -0,0 +1,10 @@
+# multiply.py
+# Author: Nur Bujang
+# This is to output what 111*555 is
+
+answer=111*555
+# nothing happened. Why? Fix it.
+# no output because this is not Interactive Python. So use print()
+
+print(answer)
+# 61605
