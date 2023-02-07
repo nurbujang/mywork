df924007cc4a0af8d45f53a1c9b9c3cde1b891d5
nur bujang <>
2023-02-07 14:27:31 +0000

    week02 lab 2.2

---
+++ week02/nameAndAge.py
@@ -0,0 +1,19 @@
+# nameAndAge.py
+# Author: Nur Bujang
+# reads in a name and age and outputs Hello Andrew, your age is 21.
+# (make sure you get the spaces right)
+
+# name=input("Enter your name:")
+# age=input("Enter your age:")
+# print (f'Hello {name}, your age is {age}')
+
+# modify the program so that it has a tab at the end of the name, (\t)
+# print (f'Hello {name}, \tyour age is {age}')
+
+# write a program that outputs 21-4 (you should get 17)
+# answer=21-4
+# print(answer)
+
+# write a program that outputs if 2==3 (you should get False)
+answer=2==3
+print(answer)
