74225f209ab734b28c1b23db8fc894de728c6d83
nur bujang <>
2023-02-07 13:53:55 +0000

    week02 inputs

---
+++ week02/addOne.py
@@ -0,0 +1,8 @@
+# addOne.py
+# Author: Nur Bujang
+# reads on a number and prints out one more than that number
+
+number=int(input('please enter an number:'))
+newNumber=number+1
+print(f'{number} plus one is {newNumber}')
+
---
+++ week02/hello2.py
@@ -0,0 +1,15 @@
+# hello2.py
+# Author: Nur Bujang
+# reads in a person's name and prints out hello that person's name
+
+# name=input("Enter your name:")
+# print('Hello '+name)
+
+# name=input("Enter your name:")
+# print('Hello'+name)
+
+# modify to say nice to meet you after saying hello
+name=input("Enter your name:")
+# print('Hello '+name+ '\nNice to meet you')
+# or we could do this with format
+print(f'Hello {name}\nNice to meet you')
