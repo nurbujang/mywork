0189b47f0b41e43bc54985f3925a0300042b2db7
nur bujang <>
2023-02-26 14:20:19 +0000

    week lecture

---
+++ week5/messingWithLists.py
@@ -0,0 +1,30 @@
+# messingWithLists.py
+# messing with lists
+# Author: Nur Bujang
+
+# boats=['Sigma', 23, [1,2,3], {}]  # {} is a function
+# boats=boats+[1,2,3]
+# print(boats)
+'''
+# if u want to use For loop
+boats=['Sigma', 23, [1,2,3], {}]  # {} is a function
+for boat in boats:
+    print(type(boat))
+'''
+
+ages=[12,21,23,34, "dfas"]
+sum=0
+for age in ages:
+    sum += age
+print(sum)
+
+# list = [2, 4, 5, 6]
+# sum = 0
+# for x in list:
+#     sum += x
+#     print (sum)
+
+# wrong way
+#for i in range(0,len(list)):
+#    print ("value of index ", i, " is ", list[i] )
+
