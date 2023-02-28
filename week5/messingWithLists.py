7c79b5df2c50f032916534f0e24f620b4052a70c
nur bujang <>
2023-02-28 11:29:02 +0000

    week 5 lecture

--- week5/messingWithLists.py
+++ week5/messingWithLists.py
@@ -12,11 +12,11 @@ for boat in boats:
     print(type(boat))
 '''
 
-ages=[12,21,23,34, "dfas"]
-sum=0
-for age in ages:
-    sum += age
-print(sum)
+# ages=[12,21,23,34, "dfas"]
+# sum=0
+# for age in ages:
+#     sum += age
+# print(sum)
 
 # list = [2, 4, 5, 6]
 # sum = 0
@@ -28,3 +28,21 @@ print(sum)
 #for i in range(0,len(list)):
 #    print ("value of index ", i, " is ", list[i] )
 
+'''
+# dont do this:
+length_of_list=len(ages)
+for i in range (0,length_of_list):
+    sum+=ages[i]
+print(sum)
+'''
+
+# string="1234567890"
+# print(string[6:])
+
+
+# t=(1,2,3)
+# print(t)
+
+t=(1,2,3)
+x,y,z=t
+print(y)
