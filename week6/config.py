cf90b9919d84eb5425c2576d194ac99141ab16bb
nur bujang <>
2023-03-04 17:35:22 +0000

    week 6 lecture

---
+++ week6/config.py
@@ -0,0 +1,9 @@
+# configuration files
+# u have gmail and u want to have a password for it
+# Author: Nur Bujang
+
+# make a dict object for gmail
+gmail={
+    "password": "hidden",
+    "username": "andrew" 
+}
