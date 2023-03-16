f0ba8980169a26d6932c4f633bac0a226dff0a9c
nur bujang <>
2023-03-16 19:17:21 +0000

    week 08 lab

--- week08/lab8-15.py
+++ week08/lab8-15.py
@@ -2,21 +2,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-import numpy as np
-import matplotlib.pyplot as plt
 minSalary = 20000
 maxSalary = 80000
 numberOfEntries = 100
+
 # this is so that the "random" numbers are the same each time
 np.random.seed(1)
 
 # create data
-ages = np.random.normal(low=21, high=65, size=numberOfEntries)
-salaries = np.random.normal(minSalary, maxSalary, numberOfEntries)
+ages = np.random.normal(size=1000)
+salaries = np.random.normal(loc=21, scale=30, size=1000)
 
 #create normal distribution curve
-sns.displot(salaries, ages)
-
+sns.displot(salaries, kde=True)
+plt.show()
+'''
 #plt.show() # if you do this the proram will halt here until the plot is closed
 # add x squared
 xpoints = np.array(range(1, 101))
@@ -28,3 +28,4 @@ plt.ylabel("age")
 plt.legend()
 plt.show() # see how the axis have changed
 #plt.savefig('prettier-plot.png')
+'''
